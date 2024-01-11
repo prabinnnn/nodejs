6bd1c26894fde5c375c16db9354929749dbd3a80
prabinnnn <prabinbhandari120@gmail.com>
2024-01-11 17:33:13 +0545

    add index1

---
+++ libaray/add.js
@@ -0,0 +1,11 @@
+const sum = () => {
+  return a + b;
+};
+console.log(sum(20, 30));
+const mul = () => {
+  return a * b;
+};
+const Sub = () => {
+  return a - b;
+};
+module.exports = { sum, mul, Sub };
